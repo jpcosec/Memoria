4ac84f4694bc4204df7865c9033594dba65aa47e
jp_ruiz <juanpablo.ruiz.r@gmail.com>
2019-09-25 18:22:06 -0300

    Update teacher_train.py

--- Mnist/teacher_train.py
+++ Mnist/teacher_train.py
@@ -15,42 +15,45 @@ data_folder = './data'
 train_model = True
 
 
+def main():
 
+  torch.set_default_tensor_type('torch.cuda.FloatTensor')
 
-torch.set_default_tensor_type('torch.cuda.FloatTensor')
+  torch.cuda.current_device()
+  device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+  print('Using device:', device)
 
-torch.cuda.current_device()
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-print('Using device:', device)
+  # Load MNIST
 
-# Load MNIST
+  train_data = MNIST(data_folder, train=True, download=True, transform=transforms.Compose([
+    transforms.ToTensor(),  # ToTensor does min-max normalization.
+  ]), )
 
-train_data = MNIST(data_folder, train=True, download=True, transform=transforms.Compose([
-  transforms.ToTensor(),  # ToTensor does min-max normalization.
-]), )
+  test_data = MNIST(data_folder, train=False, download=True, transform=transforms.Compose([
+    transforms.ToTensor(),  # ToTensor does min-max normalization.
+  ]), )
 
-test_data = MNIST(data_folder, train=False, download=True, transform=transforms.Compose([
-  transforms.ToTensor(),  # ToTensor does min-max normalization.
-]), )
+  # Create DataLoader
+  dataloader_args = dict(shuffle=True, batch_size=64, num_workers=1, pin_memory=True)
+  train_loader = dataloader.DataLoader(train_data, **dataloader_args)
+  test_loader = dataloader.DataLoader(test_data, **dataloader_args)
 
-# Create DataLoader
-dataloader_args = dict(shuffle=True, batch_size=64, num_workers=1, pin_memory=True)
-train_loader = dataloader.DataLoader(train_data, **dataloader_args)
-test_loader = dataloader.DataLoader(test_data, **dataloader_args)
+  # Instantiate net
+  net = Net().to(device)
 
-# Instantiate net
-net = Net().to(device)
+  if train_model:
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+    train(net, train_loader, optimizer, device)
+    save_model = True
+    if (save_model):
+      torch.save(net.state_dict(), model_path)
 
-if train_model:
-  optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-  train(net, train_loader, optimizer, device)
-  save_model = True
-  if (save_model):
-    torch.save(net.state_dict(), model_path)
+  else:
+    net.load_state_dict(torch.load(model_path))
 
-else:
-  net.load_state_dict(torch.load(model_path))
+  net.eval()
 
-net.eval()
+  print("net_accuracy", test(net, test_loader))
 
-print("net_accuracy", test(net, test_loader))
+if __name__ == '__main__':
+  main()
