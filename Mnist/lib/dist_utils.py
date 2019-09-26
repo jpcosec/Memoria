90a0df2ebb098afbee57803468d539260da50b22
jp_ruiz <juanpablo.ruiz.r@gmail.com>
2019-09-26 11:38:12 -0300

    Update dist_utils.py

--- Mnist/lib/dist_utils.py
+++ Mnist/lib/dist_utils.py
@@ -1,3 +1,6 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
 def dist_loss_gen(T=8):
   def dist_loss(student_scores, teacher_scores, T=T):
     return nn.KLDivLoss()(F.log_softmax(student_scores / T, dim=1), F.softmax(teacher_scores / T, dim=1))
