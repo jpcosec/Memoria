ea6497fbbbe29de07f321125d61fc248284f2a10
jp_ruiz <juanpablo.ruiz.r@gmail.com>
2019-10-01 18:34:37 -0300

    debuggin

--- Mnist/MLP_Distillation.py
+++ Mnist/MLP_Distillation.py
@@ -11,27 +11,25 @@ import torch
 from torch.autograd import Variable
 from lib.dist_model import linear_model
 from lib.dist_utils import dist_loss_gen, train_op
-from lib.utils import sample, get_dataloaders, test
+from lib.utils import sample, get_dataloaders, acc_test
 from lib.teacher_model import Net as TeacherNet
 
+from torch.utils.tensorboard import SummaryWriter
 
-def dist_model(teacher, distiller, eval_criterion, params, device, loaders):
+
+
+def dist_model(teacher, distiller, eval_criterion, params, device, loaders,writer):
   train_loader, test_loader = loaders
 
-  history = {"epoch": [],
-             "train": [],
-             "test": [],
-             "loss": [],
-             "acc_train": [],
-             "acc_test": []
-             }
 
   for epoch in range(params.epochs):
     for batch_idx, (data, target) in enumerate(train_loader):  # 784= 28*28
       loss = train_op(distiller, teacher, data, target, device)
       logger.debug(str(loss))
 
+
     if epoch % 1 == 0:
+
       x_train, y_train = sample(train_loader)
 
       y_pred = distiller["model"](x_train.view(-1, 784))
@@ -45,31 +43,33 @@ def dist_model(teacher, distiller, eval_criterion, params, device, loaders):
       test_statsT = eval_criterion(y_predT.squeeze(), y_test)
 
       # todo JSONEAR O ALGO
-      history["epoch"].append(epoch)
-      history["loss"].append(loss.item())
-      history["test"].append(test_stats.item())
-      history["train"].append(train_stats.item())
-      history["acc_test"].append(test(distiller["model"], train_loader, flatten=True))
-      history["acc_train"].append(test(distiller["model"], test_loader, flatten=True))
-      # print('Epoch {}: train loss: {}, test loss: {}'.format(epoch, loss.item(), test_stats.item()) )
 
-  return history
+      writer.add_scalar('dist-loss/train',loss.item)
+      writer.add_scalar('CE/train',train_stats)
+      writer.add_scalar('CE/test', test_stats)
+      writer.add_scalar('Acc/train', acc_test(distiller["model"], train_loader, flatten=True))
+      writer.add_scalar('Acc/test', acc_test(distiller["model"], test_loader, flatten=True))
 
 
-def distillation_experiment(neuronas, teacher, device, loaders, params):
-  exps = {}
-  dist_models = {}
+
+
+def distillation_experiment(neuronas, teacher, device, loaders, params, writer):
+
 
   for i in neuronas:
-    trains = []
-    tests = []
-    losses = []
-    acc_train = []
-    acc_test = []
+
 
     for x in range(params.experiments):
-      stexp = "\r" + str(i) + str(x)
+
+      # Lambdear
+      stexp=""
+      for neurona in i:
+        stexp+=str(neurona)+"-"
+      stexp+="_exp"+x
+      #stexp =str(i) + str(x)#"\r"
       logger.info("experimento " + stexp)
+
+      writer = SummaryWriter(comment=stexp)
 
       student_model = linear_model([i]).to(device)
       criterion = dist_loss_gen(params.temp)
@@ -78,30 +78,15 @@ def distillation_experiment(neuronas, teacher, device, loaders, params):
 
       distiller=dict(model=student_model,criterion=criterion,optimizer=optimizer)
 
-      history = dist_model(teacher, distiller, eval_criterion, params, device,
-                           loaders)
+      dist_model(teacher, distiller, eval_criterion, params, device, loaders, writer)
 
-      trains.append(history["train"])
-      tests.append(history["test"])
-      losses.append(history["loss"])
-      acc_train.append(history["acc_train"])
-      acc_test.append(history["acc_test"])
 
-    exps[i] = {"epoch": history["epoch"],
-               "train": np.array(trains).mean(axis=0),
-               "test": np.array(tests).mean(axis=0),
-               "loss": np.array(losses).mean(axis=0),
-               "acc_train": np.array(acc_train).mean(axis=0),
-               "acc_test": np.array(acc_test).mean(axis=0)
-               }
-    # models[i] = student_model
-
-  # plot_exp(exps)
-  return exps
 
 
 def main(params):
   neuronas = [int(i) for i in np.exp2(np.arange(0, 10))]
+
+
 
   torch.set_default_tensor_type('torch.cuda.FloatTensor')
   torch.cuda.current_device()
@@ -118,12 +103,8 @@ def main(params):
   for param in teacher.parameters():
     param.requires_grad = False
 
-  ex = distillation_experiment(neuronas, teacher, device, loaders, params)
+  distillation_experiment(neuronas, teacher, device, loaders, params)
 
-  p = pd.DataFrame.from_dict(ex)
-
-  with open("expDist%f.csv" % params.temp, "w") as text_file:
-    text_file.write(p.to_csv(index=True))
 
 
 if __name__ == '__main__':
