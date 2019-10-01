249cde8f981b472082eccfd9b904f504cf65c65a
jp_ruiz <juanpablo.ruiz.r@gmail.com>
2019-10-01 18:41:16 -0300

    debuggin

--- Mnist/MLP_Distillation.py
+++ Mnist/MLP_Distillation.py
@@ -53,7 +53,7 @@ def dist_model(teacher, distiller, eval_criterion, params, device, loaders,write
 
 
 
-def distillation_experiment(neuronas, teacher, device, loaders, params, writer):
+def distillation_experiment(neuronas, teacher, device, loaders, params):
 
 
   for i in neuronas:
