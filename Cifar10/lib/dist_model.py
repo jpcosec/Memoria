0fe36490e56a481372797c01c1237995d45b7f30
jp_ruiz <juanpablo.ruiz.r@gmail.com>
2019-10-04 16:30:53 -0300

    teacher_net adapting

--- Cifar10/lib/dist_model.py
+++ Cifar10/lib/dist_model.py
@@ -7,6 +7,8 @@ def linear_model(hidden_size, input_size=3072, out_size=10):
   for i in range(len(layers) - 2):
     mod_lays.append(nn.Linear(layers[i], layers[i + 1]))
     mod_lays.append(nn.ReLU())
+    nn.Dropout(0.5)
   mod_lays.append(nn.Linear(layers[-2], layers[-1]))
+  nn.Dropout(0.5)
 
   return nn.Sequential(*mod_lays)
