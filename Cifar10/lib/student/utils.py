f657e59b471424fba5f19218a67004e1e75db219
jp_ruiz <juanpablo.ruiz.r@gmail.com>
2019-10-09 19:06:46 -0300

    Both losses inclusion

--- Cifar10/lib/student/utils.py
+++ Cifar10/lib/student/utils.py
@@ -23,8 +23,7 @@ class distillation_experiment():# TODO: solucionar problemas de herencia
     self.testloader = kwargs["testloader"]
     self.trainloader = kwargs["trainloader"]
     self.best_acc = kwargs["best_acc"]
-    self.net = self.student
-# class distiller()
+    self.net = self.student)
 
 def load_teacher(args,device):
 
