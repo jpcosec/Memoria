1f73a9788ed78da7ec744f2e4256b3c94093251b
jp_ruiz <juanpablo.ruiz.r@gmail.com>
2019-10-09 11:42:29 -0300

    teacher_net adapting

--- Cifar10/student_train.py
+++ Cifar10/student_train.py
@@ -36,12 +36,7 @@ if __name__ == '__main__':
 
   trainloader, testloader, classes = load_dataset(args)
   teacher=load_teacher(args,device)
-
   student, best_acc, start_epoch = load_student(args,device)
-
-
-
-
   writer = SummaryWriter("student_trainer")
 
   criterion = dist_loss_gen(args.temp)
