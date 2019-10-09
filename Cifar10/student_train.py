c5bfbbeaec2bac5b59fac4cc4f5c4f998192d7ca
jp_ruiz <juanpablo.ruiz.r@gmail.com>
2019-10-09 11:35:11 -0300

    teacher_net adapting

--- Cifar10/student_train.py
+++ Cifar10/student_train.py
@@ -19,6 +19,7 @@ from lib.student.utils import *
 if __name__ == '__main__':
 
   parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
+  parser.add_argument("--temp", type=float, default=3.5)
   parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
   parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint', )
   parser.add_argument('--epochs', default=500, type=int, help='total number of epochs to train')
@@ -26,7 +27,7 @@ if __name__ == '__main__':
   parser.add_argument('--test_batch_size', default=100, type=int, help='total number of epochs to train')
   parser.add_argument('--student_model', default="ResNet101",
                       help="default ResNet18, other options are VGG, ResNet50, ResNet101, MobileNet, MobileNetV2, ResNeXt29, DenseNet, PreActResNet18, DPN92, SENet18, EfficientNetB0, GoogLeNet, ShuffleNetG2, ShuffleNetV2 or linear_laysize1,laysize2,laysizen")
-  parser.add_argument('--teacher_model', default="ResNet18",
+  parser.add_argument('--teacher_model', default="ResNet101",
                       help="default ResNet18, other options are VGG, ResNet50, ResNet101, MobileNet, MobileNetV2, ResNeXt29, DenseNet, PreActResNet18, DPN92, SENet18, EfficientNetB0, GoogLeNet, ShuffleNetG2, ShuffleNetV2 or linear_laysize1,laysize2,laysizen")
 
   args = parser.parse_args()
@@ -35,15 +36,17 @@ if __name__ == '__main__':
 
   trainloader, testloader, classes = load_dataset(args)
   teacher=load_teacher(args,device)
-  net, best_acc, start_epoch = load_model(args,device)
-  teacher
+
+  student, best_acc, start_epoch = load_student(args,device)
+
 
 
 
   writer = SummaryWriter("teacher_trainer")
-  criterion = nn.CrossEntropyLoss()
-  # optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
-  optimizer = optim.Adam(net.parameters(), lr=args.lr)
+
+  criterion = dist_loss_gen(args.temp)
+  eval_criterion = torch.nn.CrossEntropyLoss()
+  optimizer = optim.Adam(student.parameters(), lr=args.lr)
 
   flatten=args.model.split("_")[0] == "linear"
   exp=experiment(device=device,
