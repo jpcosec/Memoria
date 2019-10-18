ba5afece0debb1f182d6bd63fd2647504722bd12
jp_ruiz <juanpablo.ruiz.r@gmail.com>
2019-10-18 16:48:16 -0300

    Fitnets distillation inclusion

--- lib/feature_distillators/utils.py
+++ lib/feature_distillators/utils.py
@@ -46,14 +46,14 @@ class HintExperiment(DistillationExperiment):
                         self.student_features[m]=o
                     layer.register_forward_hook(hook)
 
-
-        sf = [f[1] for f in self.student_features.items()]
-        tf = [f[1] for f in self.teacher_features.items()]
         inp = torch.rand(1, 3, 32, 32).to(self.device)
         self.teacher.eval()
         self.student.eval()
         out = self.teacher(inp)
         out2 = self.student(inp)
+
+        sf = [f[1] for f in self.student_features.values()]
+        tf = [f[1] for f in self.teacher_features.items()]
 
         print(sf)
         self.regressors = [torch.nn.Conv2d(sf[i].shape[1],tf[i].shape[1],
