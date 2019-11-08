fdc172d75c7314b497278c45d5f4615f67f6110e
jpcosec <juanpablo.ruiz.r@gmail.com>
2019-11-08 14:38:20 -0300

    pre- new losses adding

--- attention.sh
+++ attention.sh
@@ -1,4 +1,4 @@
-#python Att_distillation.py --distillation=attention-2-2
-#python Att_distillation.py --distillation=attention-2-3
+python Att_distillation.py --distillation=attention-2-2
+python Att_distillation.py --distillation=attention-2-3
 python Att_distillation.py --distillation=attention-2-4 --resume
 python Att_distillation.py --distillation=attention-2-5
