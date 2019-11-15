da0aedfd19b931dafa9354d7fa899e0432647884
jpcosec <juanpablo.ruiz.r@gmail.com>
2019-11-15 13:35:35 -0300

    pre- new losses adding

--- lib/utils/records_collector.py
+++ lib/utils/records_collector.py
@@ -16,48 +16,65 @@ def maj_key(d):
         return maj
 
 
+def collect_records(folder):
+    # Collects all records and returns pandas shaped dict
+    list_jsons = glob.glob(folder + "/**/record.json", recursive=True)
+
+    info = {}
+
+    def add_entry(name, field,i,d):
+        if name not in info.keys():
+            info[name] = []
+            if i>0:
+                for i in range(i):
+                    info[name].append("")
+        info[name].append(field)
+        d[name]=False
+
+    for i,file_name in enumerate(list_jsons):
+
+        fill_dict=dict([(k, True) for k in info.keys()])
+
+        with open(file_name, 'r') as fp:
+            record = json.load(fp)
+
+        with open(file_name.replace("record", 'config'), 'r') as fp:
+            config = json.load(fp)
+
+        k = maj_key(record["test"])
+        for key, value in record["test"][str(k)].items():
+            add_entry("test_" + key, value,i,fill_dict)
+        k = maj_key(record["train"])
+        for key, value in record["train"][str(k)].items():
+            add_entry("train_" + key, value,i,fill_dict)
+
+        add_entry('epoch', record['epoch'],i,fill_dict)
+
+        for key, value in config.items():
+            add_entry(key,value,i,fill_dict)
+
+
+        for key, value in fill_dict.items():
+            if value:
+                info[key].append("")
+
+
+
+    return info
 
 if __name__ == '__main__':
     #folder="/home/jp/Memoria/repo/Cifar10/ResNet101/exp1/students"
     folder = "/home/jp/Memoria/repo/Cifar10/ResNet101/students"
-    list_jsons = glob.glob(folder+"/**/record.json", recursive=True)
-    #list_jsons = glob.glob("/home/jp/Memoria/repo/Cifar10/ResNet101/student/**/record.json", recursive=True)
-    info = {}
 
-    def add_entry(name, field):
-        if name not in info.keys():
-            info[name] = []
-        info[name].append(field)
+    info=collect_records(folder)
 
 
-    for file_name in list_jsons:
 
+    for key, value in info.items():
+        print(key, len(value))
 
-        with open(file_name, 'r') as fp:
-            record = json.load(fp)
-        #with open(file_name.replace("record", 'config'), 'r') as fp:
-        #    config = json.load(fp)
-        print(file_name)
-        k = maj_key(record["test"])
-        for key, value in record["test"][str(k)].items():
-            add_entry("test_" + key, value)
-        k = maj_key(record["train"])
-        for key, value in record["train"][str(k)].items():
-            add_entry("train_" + key, value)
+    print(list(zip(info["layer"],info["lambda"],info["distillation"])))
 
-        add_entry('epoch', record['epoch'])
-
-        dist=file_name.split("/")[-2].split("-")
-        add_entry('lambda', float(dist[1]))
-        add_entry('layer', int(dist[2]))
-        add_entry('distillation', dist[0])
-
-        #add_entry('student',config['student'])
-        #add_entry('distillation',config['distillation'].split(",")[0])
-
-        #temp = float(config['distillation'].split(",")[1].replace("T-",""))
-        #print(temp)
-        #add_entry('temp', temp)
-
+    print(info)
     csv=pd.DataFrame.from_dict(info)
     csv.to_csv(folder+"/summary.csv")
