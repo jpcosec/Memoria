de7c124e8d4454f81da7b1bd01dd3c4e214ee4fd
jp_ruiz <juanpablo.ruiz.r@gmail.com>
2019-12-09 17:15:06 -0300

    Include Original_Autoencoder

--- lib/utils/utils.py
+++ lib/utils/utils.py
@@ -71,7 +71,7 @@ def load_cifar10(args, transform_train=None, transform_test=None):
       transform_train = transforms.Compose([
           transforms.RandomCrop(32, padding=4),
           transforms.RandomHorizontalFlip(),
-          transforms.Lambda(random_return),
+          #transforms.Lambda(random_return),
           transforms.ToTensor(),
           transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 
