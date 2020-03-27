87a0b13ec2335450450e4aa89e0aaf9af7a4c90f
jp_ruiz <juanpablo.ruiz.r@gmail.com>
2020-03-27 18:34:26 -0300

    GAN integration

--- lib/Artificial_Dataset_generators/DCVAE_CIFAR10/sample.py
+++ lib/Artificial_Dataset_generators/DCVAE_CIFAR10/sample.py
@@ -5,11 +5,11 @@ import os
 
 import torch
 import torch.utils.data
-from torchvision.utils import save_image
 
-from lib.utils.funcs import auto_change_dir
-from lib.Artificial_Dataset_generators.Autoencoders.utils import load_dataset
-from lib.Artificial_Dataset_generators.Autoencoders.conv_VAE import VAE
+
+from lib.utils.funcs import check_folders
+from lib.Artificial_Dataset_generators.DCVAE_CIFAR10.utils import save_samples
+from lib.Artificial_Dataset_generators.DCVAE_CIFAR10.conv_VAE import VAE
 
 os.chdir("../../../Cifar10")
 print(os.getcwd())
@@ -17,44 +17,31 @@ print(os.getcwd())
 parser = argparse.ArgumentParser(description='VAE MNIST Example')
 parser.add_argument('--batch-size', type=int, default=128, metavar='N',
                     help='input batch size for training (default: 128)')
-parser.add_argument('--epochs', type=int, default=802, metavar='N',
+parser.add_argument('--epoch', type=int, default=100, metavar='N',
+                    help='epoch to sample')
+parser.add_argument('--n_samples', type=int, default=100000, metavar='N',
                     help='number of epochs to train (default: 10)')
-parser.add_argument('--no-cuda', action='store_true', default=False,
-                    help='enables CUDA training')
 parser.add_argument('--seed', type=int, default=1, metavar='S',
                     help='random seed (default: 1)')
-parser.add_argument('--log-interval', type=int, default=10, metavar='N',
-                    help='how many batches to wait before logging training status')
 parser.add_argument('--folder',  default="VAE-Dataset",
                     help='output folder')
 args = parser.parse_args()
-args.cuda = not args.no_cuda and torch.cuda.is_available()
 
-torch.manual_seed(args.seed)
 
 device = torch.device("cuda" if args.cuda else "cpu")
-print(device)
-kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
+
+check_folders(["samples"])
 
 
-test_loader , train_loader = load_dataset(args,kwargs)
-
-
-auto_change_dir(args.folder)
-auto_change_dir("results")
-os.chdir("..")
-
-
-
-
-
+""" Load dataset
+"""
 model = VAE().to(device)
 
 assert os.path.isdir("checkpoint")  # todo: cambiar a non initialized
 # Load checkpoint.
 print('==> Resuming from checkpoint..')
 
-checkpoint = torch.load('./checkpoint/ckpt.pth')
+checkpoint = torch.load('./checkpoints/ckpt%i.pth'%args.epoch)
 model.load_state_dict(checkpoint['net'])
 start_epoch = checkpoint['epoch']
 
@@ -63,11 +50,12 @@ start_epoch = checkpoint['epoch']
 def main():
 
     with torch.no_grad():
-        for i in range(args.n_samples//64):
-            sample = torch.randn(64, 128).to(device)
+        for i in range(args.n_samples//args.batch_size):
+            sample = torch.randn(args.batch_size, 128).to(device)
             sample = model.decode(sample).cpu()
-            save_image(sample.view(64, 3, 32, 32),
-                       'results/sample.png')
+            save_samples(sample.view(args.batch_size, 3, 32, 32),
+                        start=i*args.batch_size,
+                         batch_size=args.batch_size)
 
 if __name__ == "__main__":
     main()
