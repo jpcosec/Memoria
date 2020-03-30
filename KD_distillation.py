4ac0097251ca19907336daf38955080e04fa5b87
jpcosec <juanpablo.ruiz.r@gmail.com>
2020-03-30 00:09:45 -0300

    new experiments added

--- KD_distillation.py
+++ KD_distillation.py
@@ -49,15 +49,15 @@ def main(args):
     for epoch in range(start_epoch, args.epochs):
         exp.train_epoch()
         exp.test_epoch()
-
+    exp.save_model()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
     parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
     parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint', )  # change to restart
-    parser.add_argument('--epochs', default=100, type=int, help='total number of epochs to train')
+    parser.add_argument('--epochs', default=50, type=int, help='total number of epochs to train')
     parser.add_argument('--train_batch_size', default=128, type=int, help='batch size on train')
-    parser.add_argument('--test_batch_size', default=100, type=int, help='batch size on test')
+    parser.add_argument('--test_batch_size', default=128, type=int, help='batch size on test')
     parser.add_argument('--student', default="ResNet18",
                         help="default ResNet18, other options are VGG, ResNet50, ResNet101, MobileNet, MobileNetV2, "
                              "ResNeXt29, DenseNet, PreActResNet18, DPN92, SENet18, EfficientNetB0, GoogLeNet, "
