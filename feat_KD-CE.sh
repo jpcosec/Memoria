67c78a8dc767700dfbd3aab906e8baa81a25a079
jpcosec <juanpablo.ruiz.r@gmail.com>
2020-04-21 17:36:15 -0400

    new experiments added

--- feat_KD-CE.sh
+++ feat_KD-CE.sh
@@ -1,3 +1,11 @@
+python feat_distillation.py --distillation=hint --layer=0 --student=MobileNet --student_layer=6 --teacher_layer=26 --dataset=VAE --last_layer=KD --exp_name=feat_VAE
+python feat_distillation.py --distillation=hint --layer=1 --student=MobileNet --student_layer=15 --teacher_layer=56 --dataset=VAE --last_layer=KD --exp_name=feat_VAE
+python feat_distillation.py --distillation=hint --layer=2 --student=MobileNet --student_layer=26 --teacher_layer=219 --dataset=VAE --last_layer=KD --exp_name=feat_VAE
+python feat_distillation.py --distillation=hint --layer=3 --student=MobileNet --student_layer=55 --teacher_layer=239 --dataset=VAE --last_layer=KD --exp_name=feat_VAE
+python feat_distillation.py --distillation=hint --layer=0 --student=ResNet18 --student_layer=10 --teacher_layer=26 --dataset=VAE --last_layer=KD --exp_name=feat_VAE
+python feat_distillation.py --distillation=hint --layer=1 --student=ResNet18 --student_layer=23 --teacher_layer=56 --dataset=VAE --last_layer=KD --exp_name=feat_VAE
+python feat_distillation.py --distillation=hint --layer=2 --student=ResNet18 --student_layer=35 --teacher_layer=219 --dataset=VAE --last_layer=KD --exp_name=feat_VAE
+python feat_distillation.py --distillation=hint --layer=3 --student=ResNet18 --student_layer=46 --teacher_layer=239 --dataset=VAE --last_layer=KD --exp_name=feat_VAE
 
 
 python feat_distillation.py --distillation=nst_linear --layer=0 --student=ResNet18 --student_layer=10 --teacher_layer=26 --dataset=cifar10 --dataset=KD_CE --exp_name=feat_KD-CE
